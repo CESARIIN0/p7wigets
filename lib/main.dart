b1b678ffbab1b31daea3953106fbfa5396babeea
Cesar_Reyes_V <cesarreyesv2022@gmail.com>
2025-02-20 19:34:27 +0000

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const Mywidgets());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class Mywidgets extends StatelessWidget {
+  const Mywidgets({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Cesarin cahondin",
+                style: TextStyle(color: Colors.orangeAccent)),
+            backgroundColor: Colors.indigo,
+            centerTitle: true,
+          ),
+          body: const Column(
+            children: <Widget>[
+              Text(
+                'Cesar Reyes Vazquez',
+                style: TextStyle(color: Colors.lightBlue, fontSize: 25),
+              ),
+              Text(
+                'MAT:22308051281294 gpo 6.-J',
+                style: TextStyle(color: Colors.lightBlue, fontSize: 25),
+              ),
+              Expanded(
+                child: FittedBox(
+                  child: FlutterLogo(),
+                ),
+              ),
+            ],
+          )),
+    ); //fin de material
+  } //build
+} // mi wigdets
